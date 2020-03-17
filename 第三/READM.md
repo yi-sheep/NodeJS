bed06b8b7b6c733b9d2bcb18ae55cd6ed8f6f0da
Your Name <you@example.com>
2020-03-17 11:13:29 +0800

    第三次上课

--- 第三/READM.md
+++ 第三/READM.md
@@ -8,10 +8,10 @@
 4. 同步函数等待调用完成后返回。
 5. 多个调用异步函数的操作顺序不能保证一致，是随机的。
 6. 如果需要保持异步函数的执行顺序就要使用回调函数的方式来进行调用后续的异步函数，但是可能会在执行的层次过多时导数[回调地狱](https://www.cnblogs.com/yu412/p/11717701.html)发生。
-7. 
+7. 使用Promise来解决“回调地狱”
 
 # Nodejs 文件操作
-[文档地址：](http://nodejs.cn/api/fs.html)
+[文档地址](http://nodejs.cn/api/fs.html)
     
 ### fs.writeFile()
 ```js
@@ -67,3 +67,55 @@ fs.readFileSync(path[, options])
     2.如果在尖括号 <> 中，表示是必选参数
 
 ---
+
+# Promise
+#### 使用
+```js
+let promise = new Promise(fun(resolve,reject){
+
+});
+```
+- fun：执行器函数(有两个形式参数用于转换状态) 函数内部放一些异步操作
+    + resolve：执行器函数的第一个形参，调用转换为完成状态。
+    + reject：执行器函数的第二个形参，调用转换为失败状态。
+1. Promise有三种状态
+    
+        初始状态：从创建出Promise对象开始就是这个状态，可以转换为其他两种状态。
+        完成状态：通过在执行器函数里的第一个形式参数(形参)调用，转换为完成状态，一但转换后就不能再转换为别的状态了。
+        拒绝/失败状态：通过在执行器函数里的第二个形式参数(形参)调用，转换为拒绝/失败状态，一但转换后就不能再转换为其他状态了。
+2. then、catch的作用
+
+        使用Promise对象调用，可以在转换为完成和失败两种状态时刻之后的逻辑函数
+    ##### then()
+    ```js
+    // 当转换为完成状态的时候执行
+    promise.then(fun(data){
+
+    });
+    ```
+    - fun：调用函数
+        + data：在执行器函数里调用resolve时传入的内容
+
+    ##### catch()
+    ```js
+    // 当转换为失败状态的时候执行 
+    promise.catch(fun(data){
+
+    });
+    ```
+    - fun：调用函数
+        + data：在执行器函数里调用reject时传入的内容
+    ---
+        then和catch都可以返回一个新的Promise对象，then和catch的返回值相对于传递进去的函数的返回值
+    ---
+    ##### 链式调用
+        链式调用：方便阅读、减少代码的冗余
+        在解决回调地狱的时候需要不断的调用resolve这样会return很多个Promise对象，链式调用就不用再去创建多个变量来接受这些对象
+
+    ```js
+    peomise
+    .then(fun(data){}) // 当最初的执行器函数中调用resolve后执行这个then
+    .then(fun(data){}) // 当上一个then中return的Proimse对象中的执行器函数中调用resolve后执行这个then
+    ...  // 后面的都是一样
+    .catch((data)=>{}); // 无论上面的所有执行器函数中哪一个调用了reject都会执行这个catch
+    ```
--- 第三/fs2.js
+++ 第三/fs2.js
@@ -1,47 +1,47 @@
 let fs = require('fs');
 // 同一个异步函数执行的顺序是随机的
-// fs.writeFile('hellofs.txt','hello fs 1',(err)=>{return});
-// fs.writeFile('hellofs.txt','hello fs 2',(err)=>{return});
-// fs.writeFile('hellofs.txt','hello fs 3',(err)=>{return});
-// fs.writeFile('hellofs.txt','hello fs 4',(err)=>{return});
+fs.writeFile('hellofs3.txt','hello fs 1',(err)=>{return});
+fs.writeFile('hellofs3.txt','hello fs 2',(err)=>{return});
+fs.writeFile('hellofs3.txt','hello fs 3',(err)=>{return});
+fs.writeFile('hellofs3.txt','hello fs 4',(err)=>{return});
 
 // writeFile 的执行步骤是先把要写入的内容先放入缓存
 // readFile 的执行步骤是直接去缓存里拿取内容 
 // 这两种在与缓存进行操作的过程可以看做还是处于同步的 而写入和读取的操作是从缓存拿内容进行和硬盘的io异步操作
-// fs.readFile('hellofs.txt',(err,data)=>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log(data)
-//     }
-// })
+fs.readFile('hellofs.txt',(err,data)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
 
 // 让写入变得不随机 (有点麻烦)回调函数越来越多就会出现一个叫做回调地狱
-// fs.writeFile('hellofs.txt','hello fs 1',(err)=>{
-//     if(err){
+fs.writeFile('hellofs.txt','hello fs 1',(err)=>{
+    if(err){
 
-//     }else{
-//         fs.writeFile('hellofs.txt','hello fs 2',(err)=>{
-//             if(err){
+    }else{
+        fs.writeFile('hellofs.txt','hello fs 2',(err)=>{
+            if(err){
 
-//             }else{
-//                 fs.writeFile('hellofs.txt','hello fs 3',(err)=>{
-//                     if(err){
+            }else{
+                fs.writeFile('hellofs.txt','hello fs 3',(err)=>{
+                    if(err){
 
-//                     }else{
-//                         fs.writeFile('hellofs.txt','hello fs 4',(err)=>{
-//                             if(err){
+                    }else{
+                        fs.writeFile('hellofs.txt','hello fs 4',(err)=>{
+                            if(err){
 
-//                             }else{
-//                                 console.log('成功4');
-//                             }
-//                         });
-//                     }
-//                 });
-//             }
-//         });
-//     }
-// });
+                            }else{
+                                console.log('成功4');
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
 
 // Promise
 let obj = new Promise((resolve,reject)=>{
@@ -50,8 +50,64 @@ let obj = new Promise((resolve,reject)=>{
     // 内部放一些异步操作
     // <pending> 初始状态 可以变成完成和失败状态 一但发生变化就不会变了
     // 完成状态 
-    resolve(); // 变成完成状态
+    // resolve(); // 变成完成状态
     // 拒绝/失败状态
-    reject(); // 变成失败状态
+    // reject(); // 变成失败状态
+    setTimeout(()=>{
+        console.log(1);
+        resolve('异步完成');
+    },1000);
+});
+
+// then和catch都可以返回一个新的Promise对象，then和catch的返回值相对于传递进去的函数的返回值
+obj
+// 当是完成状态的时候执行 data为调用resolve时传入的内容
+.then((data)=>{
+    console.log(`完成状态:${data}`);
 })
-console.log(obj)
+// 当是失败状态的时候执行 
+.catch((data)=>{
+    console.log(`失败状态:${data}`);
+});
+
+
+// 解决回调地狱
+let obj = new Promise((resolve,reject)=>{
+    fs.writeFile('hellofs.txt','hello fs 1',(err)=>{
+        if(!err){
+            console.log('3完成');
+            resolve('完成1');
+        }else{
+            reject(`失败${err}`);
+        }
+    });
+});
+
+obj
+.then((data)=>{
+    return new Promise((resolve,reject)=>{
+        fs.writeFile('hellofs3.txt','hello fs 2',(err)=>{
+            if(!err){
+                console.log('2完成');
+                resolve('完成2');
+            }else{
+                reject(`失败${err}`);
+            }
+        });
+    });
+})
+.then((data)=>{
+    return new Promise((resolve,reject)=>{
+        fs.writeFile('hellofs3.txt','hello fs 3',(err)=>{
+            if(!err){
+                console.log('3完成');
+                resolve('完成3');
+            }else{
+                reject(`失败${err}`);
+            }
+        });
+    });
+})
+.catch((data)=>{
+    console.log(`失败${data}`);
+});
